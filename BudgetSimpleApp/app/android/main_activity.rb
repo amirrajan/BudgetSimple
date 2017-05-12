6ea927cbbdfae907dc13a9213813906775930284
Amir Rajan <ar@amirraja.net>
2017-05-11 19:26:02 -0500

    device height for android

--- BudgetSimpleApp/app/android/main_activity.rb
+++ BudgetSimpleApp/app/android/main_activity.rb
@@ -1,6 +1,9 @@
 class MainActivity < Android::Support::V7::App::AppCompatActivity
   def onCreate(savedInstanceState)
     super
+
+    ViewState.platform = :android
+    ViewState.device_screen_height = device_screen_height
     UI.context = self
     Store.context = self
 
@@ -8,5 +11,21 @@ class MainActivity < Android::Support::V7::App::AppCompatActivity
     navigation = UI::Navigation.new(main_screen)
     flow_app = UI::Application.new(navigation, self)
     flow_app.start
+  end
+
+  def device_screen_height
+    return @dm.heightPixels if @dm
+
+    @dm = Android::Util::DisplayMetrics.new
+    getWindowManager.getDefaultDisplay.getMetrics @dm
+    @dm.heightPixels
+  end
+
+  def device_screen_width
+    return @dm.widthPixels if @dm
+
+    @dm ||= Android::Util::DisplayMetrics.new
+    getWindowManager.getDefaultDisplay.getMetrics @dm
+    @dm.widthPixels
   end
 end
